7b664053e7b142d7a8660b815e132b858778bd00
fconfig-me <ifconfig-me@users.noreply.github.com>
2024-04-09 19:24:06 +0300

    Create knoxss.sh

---
+++ knoxss.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+file="$1"
+key="*******your-API******"
+output_file="XSS-Scan-results.txt"
+
+while read -r line; do
+    echo "Checking $line ..."
+    response=$(curl -s https://api.knoxss.pro -d "target=$line" -H "X-API-KEY: $key")
+    if grep -q '"XSS": "true"' <<< "$response"; then
+        echo "XSS found in $line"
+        echo "$line: $response" >> "$output_file"
+    else
+        echo "No XSS found in $line"
+    fi
+done < "$file"
